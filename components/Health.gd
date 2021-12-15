f4558a09c025d4991abe026bd4a05b0907a45c3b
EamonnMR <Eamonn.m.r@gmail.com>
2021-12-15 10:00:38 -0500

    Projectiles can damage and destroy bots

--- components/Health.gd
+++ components/Health.gd
@@ -14,7 +14,6 @@ func _ready():
 		health = max_health
 
 func heal(amount):
-	assert(is_network_master())
 	if health <= max_health:
 		health += amount
 		if health >= max_health:
@@ -22,25 +21,11 @@ func heal(amount):
 		emit_signal("healed")
 
 func take_damage(damage):
-	print("Server: Took Damage")
-	assert(is_network_master())
 	if health <= 0:  # Beating a dead horse
 		return
 	
 	health -= damage
 	_signal_for_damage()
-
-func set_health(new_health):
-	assert(not is_network_master())
-	if health != new_health:
-		print("Client: Took Damage")
-		
-		if new_health > health:
-			health = new_health
-			emit_signal("healed")
-		elif new_health < health:
-			health = new_health
-			_signal_for_damage()
 
 func _signal_for_damage():
 	if health <= 0:
@@ -50,14 +35,9 @@ func _signal_for_damage():
 
 
 static func do_damage(entity, damage):
-	if entity is VehicleBody:
-		var has_node = entity.has_node("Health")
-		entity.print_tree_pretty()
-		if entity.has_node("Health"):
-			print("Do some damage to something")
-			entity.get_node("Health").take_damage(damage)
-		else:
-			print(entity.name)
-			print("Cannot damage: node does not have Health node? ", entity.has_node("Health"))
+	if entity.has_node("Health"):
+		print("Do some damage to something")
+		entity.get_node("Health").take_damage(damage)
 	else:
-		print("Hit something other than a vehicle: ", entity.get_class(), ": ", entity.name)
+		print(entity.name)
+		print("Cannot damage: node does not have Health node? ", entity.has_node("Health"))
--- entities/Building.gd
+++ entities/Building.gd
@@ -2,9 +2,10 @@ extends Spatial
 
 
 func _on_Health_damaged():
-	pass
+	print("Building took hit")
 	# TODO: Switch out graphics for smashed version, play effects
 
 func _on_Health_destroyed():
+	print("Building Destroyed")
 	queue_free()
 	# TODO: Destroyed effects, spawn debris?
