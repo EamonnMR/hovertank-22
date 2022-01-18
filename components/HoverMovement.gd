e167a6dafd3523b60b1bcef636ea640c256b50c5
EamonnMR <Eamonn.m.r@gmail.com>
2022-01-18 09:56:37 -0500

    Fixes Hover Movement

--- components/HoverMovement.gd
+++ components/HoverMovement.gd
@@ -18,18 +18,20 @@ func _ready():
 
 func _physics_process(delta):
 	var motion_and_facing = controller.get_motion_and_facing()
-	var facing = motion_and_facing[1]
-	var motion_impulse = Vector3(1, 0, 0).rotated(Vector3.UP, facing)
+	var motion_impulse = motion_and_facing[0]
+	var ideal_face = motion_and_facing[1]
 	# Drifty movement:
-	if motion_and_facing[0]:
-		motion = lerp(motion, motion_impulse, accel * delta)
+	if ideal_face != null:
+		var turn_and_is_ideal = Util.constrained_turn(parent.rotation.y, delta * turn_speed, ideal_face)
+		parent.rotation.y += turn_and_is_ideal[0]
+		var is_ideal = turn_and_is_ideal[1]
+		if motion_impulse:
+			motion = lerp(motion, Vector3(1, 0, 0).rotated(Vector3.UP, parent.rotation.y), accel * delta)
 	else:
 		motion = lerp(motion, Vector3(0,0,0), drag * delta)
 	# TODO: Lerp facing
-	if facing != null:
-		var turn_and_is_ideal = Util.constrained_turn(parent.rotation.y, delta * turn_speed, facing)
-		parent.rotation.y += turn_and_is_ideal[0]
-	match_ground_normal(delta, parent, 0.02)
+
 	var gravity_delta = gravity * delta * Vector3.DOWN
 	var motion_total = motion * motion_speed + gravity_delta
 	parent.move_and_slide_with_snap(motion_total, Vector3.DOWN, Vector3.UP)
+	match_ground_normal(delta, parent, 0.02)
