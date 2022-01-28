255000240ba3fa89e33008718cd6d2f50ea19d65
EamonnMR <Eamonn.m.r@gmail.com>
2022-01-28 17:52:34 -0500

    Impact Effects

--- projectiles/Projectile.gd
+++ projectiles/Projectile.gd
@@ -3,6 +3,7 @@ extends KinematicBody
 export var speed = 50
 export var damage = 20
 export var already_exploded = false
+export var explosion: PackedScene
 signal impact
 
 var iff: IffProfile
@@ -21,14 +22,16 @@ func _physics_process(delta):
 		
 		_do_impact(collider)
 	
-func explode():
-	# Blam!
-	pass
+func _explode():
+	# TODO: Calculate ricochet angle
+	var explo = explosion.instance()
+	explo.transform.origin = global_transform.origin
+	get_node("../").add_child(explo)
 
 func _do_impact(collider):
 	print("Shot impact - do damage")
 	emit_signal("impact")
 	if not iff.should_exclude(collider):
 		Health.do_damage(collider, damage)
-	print("Blam!")
+	_explode()
 	queue_free()
