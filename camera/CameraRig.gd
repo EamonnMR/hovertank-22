4c59aa699b82d4bb93da0abc51c660967ccfb949
EamonnMR <Eamonn.m.r@gmail.com>
2022-03-22 10:13:32 -0400

    Put FPS counter in the right place

--- camera/CameraRig.gd
+++ camera/CameraRig.gd
@@ -5,6 +5,7 @@ var aim_position: Vector3
 var aim_correct = false
 var AIM_UP_CORRECTION = Vector3(0,1,0) # We don't want to be aiming right at the ground all the time
 var zoom_target = 1.0
+var TOPDOWN_OFFSET = Vector3(0,100,50)
 export var zoom_min = 0.01
 export var zoom_max = 3.0
 export var zoom_incriment = 0.25
@@ -36,7 +37,7 @@ func _ready():
 		#current_camera.look_at($CameraOffset.to_global(Vector3(0,0,0)), Vector3.UP)
 	else:
 		current_camera = $CameraOffset/PitchHelper/CameraLocation/Camera
-		base_distance = Vector3(0,100,50)
+		base_distance = TOPDOWN_OFFSET
 		current_camera.look_at($CameraOffset.to_global(Vector3(0,0,0)), Vector3.UP)
 
 func _handle_zoom(delta: float):
