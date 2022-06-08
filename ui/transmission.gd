a0e94938460e318540554a77927d91f9aef81608
EamonnMR <Eamonn.m.r@gmail.com>
2022-06-08 19:29:35 -0400

    It's rough but it's a briefing

---
+++ ui/transmission.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready():
+	# TODO: Only trigger this sometimes
+	call_deferred("start")
+	
+func start():
+	var parent = get_node("../")
+	parent.remove_child(self)
+	Hud.add_child(self)
+	show()
+	$AudioStreamPlayer.play()
+	parent.connect("tree_exited", self, "queue_free")
+
+func _on_AudioStreamPlayer_finished():
+	queue_free()
