82245f5db002907830ea9f8bf0d3f7bc020d41c9
EamonnMR <eamonn.m.r@gmail.com>
2023-04-17 23:53:29 -0400

    Fixed exit door

--- entities/ExitDoor.gd
+++ entities/ExitDoor.gd
@@ -6,6 +6,7 @@ func _on_Area_body_entered(body):
 	if body.has_method("is_player") and body.is_player():
 		emit_signal("destroyed")
 		queue_free()
+		Client.victory_screen()
 
 func graphics():
 	return $Graphics
