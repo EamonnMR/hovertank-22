879de64bae3aee78899de77b6ea227ac55319338
EamonnMR <eamonn.m.r@gmail.com>
2024-02-02 21:48:42 -0500

    Lint a bunch of files differently

--- components/player_control/MouseAndKeyboardController.gd
+++ components/player_control/MouseAndKeyboardController.gd
@@ -8,29 +8,14 @@ class_name MouseAndKeyboardController
 
 @onready var parent = get_node("../")
 
-#func next_weapon():
-#	return false
-	
-#func previous_weapon():
-#	return false
-
 func _ready():
 	get_node("../").add_to_group("players")
 	get_node("../").connect("destroyed",Callable(self,"_on_player_destroyed"))
 func get_turn_and_motion_impulse(delta, turn_speed) -> Array:
 	return [0.0, 0]
 
-#func get_aim_point() -> Vector3:
-#	return Vector3()
-
 func is_player():
 	return true
-
-#func is_shooting():
-#	return false
-
-#func is_shooting_secondary():
-#	return false
 
 func _on_player_destroyed():
 	var timer :Timer = Timer.new()
@@ -43,8 +28,6 @@ func _on_player_destroyed():
 	
 func _get_turret_turn():
 	return parent.core.get_turrets()[0].request_turn
-
-
 
 @onready var cam_rig = get_node("../../CameraRig")
 
